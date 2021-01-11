9c06d47b307467849e57ea611ef8d0cd1d5670fa
Tim Hagn <timhagn@users.noreply.github.com>
2021-01-11 22:59:42 +0100

    Fix category notation in /ecosystem dropdown (#709)
    * add category mapping, adapt PartnerFilterDropdown
    
    * fix formatting (older prettier version)
    
    * fix formatting after prettier update
    
    * More category work
    
    Co-authored-by: R. M. Shea <8948187+rmshea@users.noreply.github.com>

---
+++ categoryMapping.js
@@ -0,0 +1,21 @@
+/**
+ * Mapping object for partner category notation (capitalized, etc.).
+ *
+ * @type {Object}
+ */
+const CATEGORY_MAPPING = {
+  amm: "AMM",
+  app: "App",
+  defi: "DeFi",
+  dex: "DEX",
+  exchange: "Exchange",
+  explorer: "Explorer",
+  infra: "Infrastructure",
+  oracle: "Oracle",
+  spl: "SPL",
+  stablecoin: "Stablecoin",
+  tools: "Tools",
+  wallet: "Wallet",
+};
+
+export default CATEGORY_MAPPING;
