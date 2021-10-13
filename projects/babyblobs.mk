b63ae795f966262ed729a30f516adaf1bbfbf604
stegaBOB <41593264+stegaBOB@users.noreply.github.com>
2021-10-13 12:58:53 -0400

    added babyblobs file

---
+++ projects/babyblobs.mk
@@ -0,0 +1,16 @@
+---
+slug: "babyblobs"
+date: "2020-10-13"
+title: "Baby Blobs"
+logline: "The first interactive NFT collection on Solana, launching on October 25th using the Metaplex Candy Machine."
+cta: "https://babyblobs.art"
+logo: /img/babyblobs.png
+category: nft, metaplex
+status: building
+website: https://babyblobs.art/
+twitter: https://twitter.com/BabyBlobs
+discord: https://discord.com/BabyBlobs
+---
+
+The first interactive NFT collection on Solana, launching on October 25th using the Metaplex Candy Machine. 
+These cute, bouncy blobs are an adorable, fun addition to your NFT portfolio!
